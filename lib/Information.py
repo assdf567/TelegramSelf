12621f70e8d0302521b3995d00de6016ea67646e
assdf567 <asoheili231@gmail.com>
2025-07-04 17:35:28 +0330

    Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,14 +1,9 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
-
-client_id = '01e7dc6b41c3471b94efe87abeb05919'
-client_secret = '4f5f93af1ced4b0d9ba8440606803639'
+admin_user_id = 6852189582  # <--- آیدی عددی ادمین
+api_id = 24597015           # <--- API ID
+api_hash = '1b1aefc2645a9275a3069ca6b7b7f7a2'  # <--- API Hash
+helper_username = 'Sellllllggfamir_bot'  # <--- یوزرنیم ربات هلپر بدون @
+bot_token = '7532564216:AAFGOHsrv19a1QfIiGJDM7nkn1uS4iVsVGk'  # <--- توکن ربات هلپر
 
 client = TelegramClient('TRself-MT', api_id, api_hash)
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
